bcac4166fafad36287260c366a1853ee34d72c68
Mary <git@mary.my.id>
2024-12-13 09:45:25 +0700

    docs: remove old site

--- packages/site/src/main.tsx
+++
@@ -1,8 +0,0 @@
-import { render } from 'preact';
-
-import App from './app';
-
-import 'bluesky-post-embed/themes/light.css';
-import './styles/main.css';
-
-render(<App />, document.body);
--- packages/site/src/utils/html.ts
+++
@@ -1,3 +0,0 @@
-export const escapeHtml = (text: string): string => {
-	return text.replace(/[<"&]/g, (c) => '&#' + c.charCodeAt(0) + ';');
-};
